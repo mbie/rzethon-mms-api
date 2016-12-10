02907484a48b896be70d44a117bfdd394266e08b
mbie <embielec@gmail.com>
2016-12-10 15:53:12 +0100

    Add uuid to messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210134136) do
+ActiveRecord::Schema.define(version: 20161210144928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
   create_table "messages", force: :cascade do |t|
-    t.string   "content",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "content",                                          null: false
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+    t.uuid     "uuid",       default: -> { "uuid_generate_v4()" }, null: false
+    t.index ["uuid"], name: "messages_uuid_key", unique: true, using: :btree
   end
 
 end
