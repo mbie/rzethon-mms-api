0662e2805553c4ee2aa37cdb4213bde185846c57
mbie <embielec@gmail.com>
2016-12-10 15:55:47 +0100

    Reload message after creating to return uuid in the response

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   def create
     message = Message.new(message_params)
     if message.save
-      render json: { message: message }
+      render json: { message: message.reload }
     else
       render json: { error: message.errors.full_messages }, status: :unprocessable_entity
     end
