b8ac1de4039733af6f55e1a99a1f2011f7fb3003
mbie <embielec@gmail.com>
2016-12-10 18:17:35 +0100

    Add endpoint for submissions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :messages, only: [:create]
   resources :nodes, only: [:index]
+  resources :submissions, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
