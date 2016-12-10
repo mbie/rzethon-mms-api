7dcd02ed1c91d30682a5b971e63c2ae9628dacb3
mbie <embielec@gmail.com>
2016-12-10 15:10:44 +0100

    Add ability to create message

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,14 @@
+class MessagesController < ApplicationController
+  def create
+    message = Message.new(message_params)
+    if message.save
+      render json: { message: message }
+    else
+      render json: { error: message.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
