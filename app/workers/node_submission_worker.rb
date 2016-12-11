1096fc2d2fbb505e1734182d844803c664ab687f
mbie <embielec@gmail.com>
2016-12-11 02:10:40 +0100

    Submit node in worker

--- app/workers/node_submission_worker.rb
+++ app/workers/node_submission_worker.rb
@@ -2,8 +2,17 @@ class NodeSubmissionWorker
   include Sidekiq::Worker
   sidekiq_options dead: false
 
-  def perform(host)
-    # client = NodeService.new(host)
-    # client.submit(host)
+  def perform(base_host, current_host, name)
+    location = NodeLocationService.new(name).get_location
+
+    node_params = {
+      host: current_host,
+      name: name
+    }.merge(location.attributes)
+
+    node = Node.create!(node_params)
+    
+    client = NodeService.new(base_host)
+    client.submit(node.as_json(except: :id))
   end
 end
--- config/initializers/node_submission.rb
+++ config/initializers/node_submission.rb
@@ -1,3 +1,8 @@
 # Submit current node to known host after application initialization
-host = "http://localhost:3001"
-NodeSubmissionWorker.perform_in(1.minute, host)
+base_host = ENV["BASE_NODE_HOST"] || "http://localhost:3000"
+current_host = ENV["CURRENT_NODE_HOST"] || "http://localhost:3001"
+name = ENV["CURRENT_NODE_NAME"] || "MARS#2.3"
+
+# if Node.count.zero? 
+  # NodeSubmissionWorker.perform_async(base_host, current_host, name)
+# end
