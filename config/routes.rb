0ac38a0eef69971f5cd9a10ff8dc6d73c4972331
wielki.borsuk <wielki.borsuk@gmail.com>
2017-03-09 09:50:25 +0100

    cleanup schema migrations, refactor route definition

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,22 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 
-  get '/nodes/names', to: 'nodes#names'
-  put '/nodes/register_name', to: 'nodes#register_name'
-  get '/nodes/me', to: 'nodes#me'
-  get '/simulations/paths', to: 'simulations#paths'
-
   resources :messages, only: [:create, :index]
-  resources :nodes, only: [:index, :show]
   resources :submissions, only: [:create]
   resources :propagations, only: [:create]
-  resources :simulations, only: [:index]
+
+  resources :nodes, only: [:index, :show] do
+    collection do
+      get :names
+      get :me
+      put :register_name
+    end
+  end
+
+  resources :simulations, only: [:index] do
+    collection do
+      get :paths
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20161210134136_create_messages.rb
+++ db/migrate/20161210134136_create_messages.rb
@@ -7,7 +7,6 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     end
 
     create_table :messages, id: :uuid do |t|
-      #t.uuid :uuid, unique: true, null: false, primary: true
       t.string :content, null: false
       t.string :source, null: false
       t.string :destination, null: false
